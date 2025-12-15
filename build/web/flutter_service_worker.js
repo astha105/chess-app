7806c88526580af31e9ba653ecf251756311f056
astha105 <agarwalastha192@gmail.com>
2025-12-15 09:52:49 +0530

    Fix chess board responsive layout for web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "49fa57ddf86ad7a264c3cf682a8ed209",
+const RESOURCES = {"flutter_bootstrap.js": "95dadcaa19a26c4783252c4ce65de8bf",
 "version.json": "ebe8e1fac52ae8e38f2af9bc55658c7f",
 "index.html": "733e4dc61ea9847f0b71e51c36dfd754",
 "/": "733e4dc61ea9847f0b71e51c36dfd754",
